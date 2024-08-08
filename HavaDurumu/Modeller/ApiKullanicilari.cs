3df79956f40a8b9759af8cb5e9b3b9595bc87e2a
oguzhancirkin <oguzhancirkin@OGUZHANCIRKIN>
2024-08-08 20:30:34 +0300

    Add project files.

---
+++ HavaDurumu/Modeller/ApiKullanicilari.cs
@@ -0,0 +1,11 @@
+﻿namespace HavaDurumu.Modeller
+{
+    public class ApiKullanicilari
+    {
+        public static List<ApiKullanicisi> Kullanicilar = new()
+       {
+           new ApiKullanicisi { Id = 1 , KullaniciAdi = "Oguzhan", Sifre = "123456", Rol = "Yönetici"},
+           new ApiKullanicisi {Id = 2 ,KullaniciAdi = "Nurullah" , Sifre = "1907", Rol ="StandartKullanici"}
+       };
+    }
+}
---
+++ HavaDurumu/Modeller/ApiKullanicisi.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Principal;
+
+namespace HavaDurumu.Modeller
+{
+    public class ApiKullanicisi
+    {
+        public int Id { get; set; }
+        public string? KullaniciAdi { get; set; }
+        public string? Sifre { get; set; }
+
+        public string? Rol { get; set; }
+    }
+}
